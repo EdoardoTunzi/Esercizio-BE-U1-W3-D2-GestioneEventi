944cd31b7079b490781f22f2310daa3ad3b87d17
Edoardo <edfl.tunzi@gmail.com>
2025-01-22 14:33:36 +0100

    Classi Persona,Partecipazione,Location + 2 enum creati con getter,setter,tostring

---
+++ src/main/java/org/example/entities/Location.java
@@ -0,0 +1,57 @@
+package org.example.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "locations")
+public class Location {
+
+    @Id
+    @GeneratedValue
+    private long id;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private String citta;
+
+    public Location() {
+    }
+
+    public Location(String nome, String citta) {
+        this.nome = nome;
+        this.citta = citta;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCitta() {
+        return citta;
+    }
+
+    public void setCitta(String citta) {
+        this.citta = citta;
+    }
+
+    @Override
+    public String toString() {
+        return "Location{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", citta='" + citta + '\'' +
+                '}';
+    }
+}
